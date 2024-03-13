258882dbf6ff8d77d2f712ceb4898bdd5f81e312
Nant1ng <andressantana99@hotmail.se>
2024-03-13 22:34:50 +0100

    Showcase done

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import Showcase from "./sections/homepage/Showcase";
 
 const Home = () => {
-  const url = "https://kyhnet23-assignment.azurewebsites.net/api/specialists";
-  const [specialists, setSpecialists] = useState([]);
+  // const url = "https://kyhnet23-assignment.azurewebsites.net/api/specialists";
+  // const [specialists, setSpecialists] = useState([]);
 
   /*   useEffect(() => {
     fetch(url)
@@ -11,29 +12,30 @@ const Home = () => {
       .catch((error) => console.error("Error fetching data: ", error));
   }, [url]); */
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await fetch(url);
-        const data = await result.json();
-        setSpecialists(data);
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const result = await fetch(url);
+  //       const data = await result.json();
+  //       setSpecialists(data);
+  //     } catch (error) {
+  //       console.error("Error fetching data: ", error);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <h1>Home</h1>
-      {/* {specialists.map((specialist, idx) => (
+  //   fetchData();
+  // }, []);
+  {
+    /* {specialists.map((specialist, idx) => (
         <h1 key={idx}>
           {idx}: {specialist?.firstName}
         </h1>
-      ))} */}
-    </div>
+      ))} */
+  }
+  return (
+    <main>
+      <Showcase />
+    </main>
   );
 };
 
---
+++ src/views/sections/homepage/Showcase.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Appstore from "../../../assets/images/appstore.svg";
+import Googleplay from "../../../assets/images/googleplay.svg";
+import ShowcaseMobiles from "../../../assets/images/Showcase-mobiles.svg";
+
+import ArrowDownIcon from "../../../assets/icons/ArrowDown.svg";
+
+const Showcase = () => {
+  return (
+    <section id="showcase">
+      <div className="container">
+        <div className="content">
+          <h1>Mange All Your Money in One App</h1>
+          <p>
+            We offer you a new generation of the mobile banking. Save, spend &
+            manage money in your pocket.
+          </p>
+          <div className="download-app">
+            <Link>
+              <img src={Appstore} alt="Appstore logo" />
+            </Link>
+            <Link>
+              <img src={Googleplay} alt="Googleplay logo" />
+            </Link>
+          </div>
+          <Link className="more">
+            <span className="icon-wrapper">
+              <img
+                src={ArrowDownIcon}
+                alt="Arrow pointing down"
+                className="icon"
+              />
+            </span>
+            <span className="link-text">Discover more</span>
+          </Link>
+        </div>
+        <img
+          id="showcase-image"
+          src={ShowcaseMobiles}
+          alt="Two mobile phones"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Showcase;
