51e70ef824cc85e692c570e25b9391dba735badd
Nant1ng <andressantana99@hotmail.se>
2024-03-15 15:41:37 +0100

    working

---
+++ src/lib/postContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext } from "react";
+
+const PostStore = createContext();
+
+export const usePostStore = () => useContext(PostStore);
+
+export const PostStoreProvider = ({ children }) => {
+  const postData = async (endpoint, body = undefined) => {
+    console.log(body)
+    const config =
+      body !== undefined
+        ? {
+            method: "post",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(body),
+          }
+        : { method: "post" };
+    const res = await fetch(`${API_BASE_URL}${endpoint}`, config);
+
+    return res;
+  };
+
+  return (
+    <PostStore.Provider value={{ postData }}>{children}</PostStore.Provider>
+  );
+};
